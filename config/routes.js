4015d15ce8ab248dff4140dada52095bed489d92
Phil Hart <phil@Phils-iMac.local>
2018-12-07 10:00:32 -0700

    Finished MVP, moving onto stretch

--- config/routes.js
+++ config/routes.js
@@ -2,6 +2,11 @@ const axios = require('axios');
 
 const { authenticate } = require('./middlewares');
 
+const bcrypt = require("bcryptjs");
+const db = require("../database/dbConfig");
+const jwtKey = require("../_secrets/keys").jwtKey;
+const jwt = require("jsonwebtoken");
+
 module.exports = server => {
   server.post('/api/register', register);
   server.post('/api/login', login);
@@ -10,10 +15,47 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+  const creds = req.body;
+  const hash = bcrypt.hashSync(creds.password, 4); 
+  creds.password = hash;
+
+  db('users')
+    .insert(creds)
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => json(err));
+
+}
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+  const secret = jwtKey;
+  const options = {
+    expiresIn: "60m"
+  };
+  return jwt.sign(payload, secret, options);
 }
 
 function login(req, res) {
   // implement user login
+  const creds = req.body;
+
+  db('users')
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ message: 'welcome!', token });
+      } else {
+        res.status(401).json({ message: 'you shall not pass!!' });
+      }
+    })
+    .catch(err => res.json(err));
 }
 
 function getJokes(req, res) {
