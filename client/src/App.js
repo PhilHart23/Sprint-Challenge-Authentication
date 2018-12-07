9b75e52ed4c622684322549beed08212d39c812d
Phil Hart <phil@Phils-iMac.local>
2018-12-07 10:38:24 -0700

    Got React up and running

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { Route, NavLink } from "react-router-dom";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Jokes from "./components/Jokes";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        
+          <nav>
+            <NavLink to="/">Sign Up</NavLink>
+            &nbsp; | &nbsp;
+            <NavLink to="/jokes">Jokes</NavLink>
+            &nbsp; | &nbsp;
+            <NavLink to="/login">Sign In</NavLink>
+          </nav>
+          <section>
+            <Route exact path="/" component={Register} />
+            <Route path="/jokes" component={Jokes} />
+            <Route path="/login" component={Login} />
+          </section>
+       
       </div>
     );
   }
